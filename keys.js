8cc1378f0256c4a89511dd947e225cab760d4ccb
Ben Sojka <bensojka@gmail.com>
2017-09-02 00:53:01 -0500

    turning in

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'zp1qFUCUxfcK7jIGAUyAxte36',
+  consumer_secret: 'P78cjraJcFOHSszvb1UsNmIfOc2XUpJXP31hd8SD3KfSO5ZKG8',
+  access_token_key: '903006210147979267-Bg8BAnTviphxTKuIySa04OSXnujdaBn',
+  access_token_secret: 'xvbmsTt1XfGOypLlqJzXViBHe6O4VGBoehOcxfVEgiRn3',
+}
+
+exports.spotifyKeys = {
+  id: '4432181f8fdb4eeca2f8586485e59e3e',
+  secret: '010b13840367441e98579c68edb8a3c8',
+}
---
+++ liri.js
@@ -0,0 +1,175 @@
+var fs = require("fs");
+
+var request = require("request");
+
+var Twitter = require("twitter");
+
+var Spotify = require("node-spotify-api");
+
+var keys = require("./keys.js");
+
+var twitterKeys = keys.twitterKeys;
+
+var inputString = process.argv;
+
+var command = inputString[2];
+
+var name = inputString[3];
+
+var movieName = "";
+
+var client;
+
+
+
+if (command === "my-tweets") {
+    client = new Twitter({
+        consumer_key: keys.twitterKeys.consumer_key,
+        consumer_secret: keys.twitterKeys.consumer_secret,
+        access_token_key: keys.twitterKeys.access_token_key,
+        access_token_secret: keys.twitterKeys.access_token_secret,
+    });
+    if(!name) {
+        name = "dadboner";
+    }
+    params = {screen_name: name};
+    client.get("statuses/user_timeline/", params, function(error, data, response){
+        if (!error) {
+            for (var i = 0; i < data.length; i++) {
+                var dStr = data[i].created_at;
+                var d = dStr.replace("+0000 ", "");
+                var twitterResults =
+                d + ": " + "@" + data[i].user.screen_name + ": " +
+                data[i].text + "\r\n" +
+                "-------------------------------" + i + "-------------------------------" + "\r\n";
+                console.log(twitterResults);
+            }
+        }
+        else {
+            console.log("Error :" + error);
+            return;
+        }
+    });
+}
+
+else if (command === "spotify-this-song") {
+    client = new Spotify({
+        id: keys.spotifyKeys.id,
+        secret: keys.spotifyKeys.secret,
+    });
+    if(!name){
+        name = "The Sign Ace of Base";
+    }
+    params = name;
+    client.search({ type: "track", query: params }, function(err, data) {
+        if(!err){
+            var songInfo = data.tracks.items;
+            for (var i = 0; i < 1; i++) {
+                if (songInfo[i] != undefined) {
+                    var spotifyResults =
+                    "Artist: " + songInfo[i].artists[0].name + "\r\n" +
+                    "Song: " + songInfo[i].name + "\r\n" +
+                    "Album: " + songInfo[i].album.name + "\r\n" +
+                    "Preview: " + songInfo[i].preview_url + "\r\n" + 
+                    "------------------------------ " + i + " ------------------------------" + "\r\n";
+                    console.log(spotifyResults);
+                }
+            }
+        }	else {
+            console.log("Error :"+ err);
+            return;
+        }
+    });
+}
+
+else if (command === "movie-this") {
+    for (var i = 3; i < inputString.length; i++) {
+        
+        if (i > 3 && i < inputString.length) {
+        
+            movieName = movieName + "+" + inputString[i];
+        
+        }
+        
+        else {
+        
+            movieName += inputString[i];
+        
+        }
+    }
+
+    if(!name){
+        movieName = "Mr+Nobody";
+    }
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=40e9cece";
+
+    console.log(queryUrl);
+    
+    request(queryUrl, function(error, response, body) {
+    
+      // If the request is successful
+        if (!error && response.statusCode === 200) {
+        
+            var movieObject = JSON.parse(body);
+            var movieResults =
+            "Title: " + movieObject.Title+"\r\n"+
+            "Year: " + movieObject.Year+"\r\n"+
+            "Imdb Rating: " + movieObject.imdbRating+"\r\n"+
+            "Country: " + movieObject.Country+"\r\n"+
+            "Language: " + movieObject.Language+"\r\n"+
+            "Plot: " + movieObject.Plot+"\r\n"+
+            "Actors: " + movieObject.Actors+"\r\n"+
+            "Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
+            "Rotten Tomatoes URL: " + movieObject.tomatoURL+ "\r\n"; 
+            console.log(movieResults);
+
+        } else {
+            console.log("Error :"+ error);
+            return;
+        }
+    });
+}
+
+else if (command === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function(error, data){
+        if (!error) {
+            var doWhatItSaysResults = data.split(",");
+            var spotifyThis = doWhatItSaysResults[1];
+
+            client = new Spotify({
+                id: keys.spotifyKeys.id,
+                secret: keys.spotifyKeys.secret,
+            });
+            params = spotifyThis;
+            client.search({ type: "track", query: params }, function(err, data) {
+                if(!err){
+                    var songInfo = data.tracks.items;
+                    for (var i = 0; i < 1; i++) {
+                        if (songInfo[i] != undefined) {
+                            var spotifyResults =
+                            "Artist: " + songInfo[i].artists[0].name + "\r\n" +
+                            "Song: " + songInfo[i].name + "\r\n" +
+                            "Album: " + songInfo[i].album.name + "\r\n" +
+                            "Preview: " + songInfo[i].preview_url + "\r\n" + 
+                            "------------------------------ " + i + " ------------------------------" + "\r\n";
+                            console.log(spotifyResults);
+                        }
+                    }
+                }	else {
+                    console.log("Error :"+ err);
+                    return;
+                }
+            });
+            
+        } 
+        else {
+            console.log("Error occurred" + error);
+        }
+    });
+}
+
+
+else {
+    console.log("Not a recognized command");
+}
